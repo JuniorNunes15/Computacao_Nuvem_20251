5c3e3d16ff678f8c338868c50eeabc5de0364a6c
Gab-Dealem04 <gabrielycorreia@alu.ufc.br>
2025-07-29 23:44:05 -0300

    changes configs db

--- backend/instance/config.py
+++ backend/instance/config.py
@@ -1,7 +1,8 @@
-# instance/config.py
+import os
 
-# Configurações do banco de dados (ex: Amazon RDS)
-DATABASE_URI = 'mysql://mob:psycho@localhost/contatos_db'
+db_user = os.getenv("MYSQL_USER", "admin")
+db_password = os.getenv("MYSQL_PASSWORD", "admin123")
+db_host = os.getenv("MYSQL_HOST", "localhost")
+db_name = os.getenv("MYSQL_DB", "contatos_db")
 
-# Configurações de ambiente
-DEBUG = False  # Em produção, desative o modo debug
+
