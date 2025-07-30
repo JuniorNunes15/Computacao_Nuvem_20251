1c22bcabbe82fdd9d2c4d705d6dc5afeb74cc3c7
JuniorNunes15 <61329437+JuniorNunes15@users.noreply.github.com>
2025-07-30 00:15:43 -0300

    Update config.py
    changes config db

--- backend/instance/config.py
+++ backend/instance/config.py
@@ -1,7 +1,8 @@
 # instance/config.py
-
+import os
+db_host = os.getenv("DB_HOST", "localhost")
 # Configurações do banco de dados (ex: Amazon RDS)
-DATABASE_URI = 'mysql://admin:admin123@localhost/contatos_db'
+DATABASE_URI = 'mysql://admin:admin123@{db_host}/contatos_db'
 
 # Configurações de ambiente
 DEBUG = False  # Em produção, desative o modo debug
